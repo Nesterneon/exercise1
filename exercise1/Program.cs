e1b1d953fa72c2a4d1e148cab10f8a8ee852f1ee
79064 <79064@MSI>
2021-11-19 12:56:15 +0300

    Добавьте файлы проекта.

---
+++ exercise1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение длинны окружности L");
+            int L = Convert.ToInt32(Console.ReadLine());
+            double R = L / 2/ Math.PI;
+            double S = Math.PI*R*R;
+            Console.WriteLine("Радиус={0}", R);
+            Console.WriteLine("Площадь={0}", S);
+            Console.Write("Для завершения нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
